9ae031632de6c1a2ab4f0891a1e9fd6cd03f46b6
FVBDAWY <117821429+FVBDAWY@users.noreply.github.com>
2023-03-08 18:04:08 +0100

    Add files via upload

---
+++ Ejercicio 8/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Ejercicio_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bNumeroAleatorio = new System.Windows.Forms.Button();
+            this.bIngresarNumeros = new System.Windows.Forms.Button();
+            this.bNumerosAcertados = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bNumeroAleatorio
+            // 
+            this.bNumeroAleatorio.Location = new System.Drawing.Point(249, 47);
+            this.bNumeroAleatorio.Name = "bNumeroAleatorio";
+            this.bNumeroAleatorio.Size = new System.Drawing.Size(249, 51);
+            this.bNumeroAleatorio.TabIndex = 0;
+            this.bNumeroAleatorio.Text = "Generar Numero Aleatorio";
+            this.bNumeroAleatorio.UseVisualStyleBackColor = true;
+            this.bNumeroAleatorio.Click += new System.EventHandler(this.bNumeroAleatorio_Click);
+            // 
+            // bIngresarNumeros
+            // 
+            this.bIngresarNumeros.Location = new System.Drawing.Point(249, 140);
+            this.bIngresarNumeros.Name = "bIngresarNumeros";
+            this.bIngresarNumeros.Size = new System.Drawing.Size(249, 43);
+            this.bIngresarNumeros.TabIndex = 1;
+            this.bIngresarNumeros.Text = "Ingresar numeros ";
+            this.bIngresarNumeros.UseVisualStyleBackColor = true;
+            this.bIngresarNumeros.Click += new System.EventHandler(this.bIngresarNumeros_Click);
+            // 
+            // bNumerosAcertados
+            // 
+            this.bNumerosAcertados.Location = new System.Drawing.Point(249, 219);
+            this.bNumerosAcertados.Name = "bNumerosAcertados";
+            this.bNumerosAcertados.Size = new System.Drawing.Size(249, 44);
+            this.bNumerosAcertados.TabIndex = 2;
+            this.bNumerosAcertados.Text = "Numeros acertados";
+            this.bNumerosAcertados.UseVisualStyleBackColor = true;
+            this.bNumerosAcertados.Click += new System.EventHandler(this.bNumerosAcertados_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bNumerosAcertados);
+            this.Controls.Add(this.bIngresarNumeros);
+            this.Controls.Add(this.bNumeroAleatorio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bNumeroAleatorio;
+        private System.Windows.Forms.Button bIngresarNumeros;
+        private System.Windows.Forms.Button bNumerosAcertados;
+    }
+}
+
---
+++ Ejercicio 8/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Ejercicio_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> numerosSorteo = new List<int>();
+        List<int> numerosUsuario = new List<int>();
+        private void bNumeroAleatorio_Click(object sender, EventArgs e)
+        {
+            numerosSorteo.Clear();         
+            while (numerosSorteo.Count < 6)
+            {
+                int numeroAleatorio = new Random().Next(1, 50);
+                if (!numerosSorteo.Contains(numeroAleatorio))
+                {
+                    numerosSorteo.Add(numeroAleatorio);
+                }
+            }
+            MessageBox.Show("Los numeros aleatorios han sido generados");
+
+        }
+
+        private void bIngresarNumeros_Click(object sender, EventArgs e)
+        {
+            numerosUsuario.Clear();
+            int numero;
+
+            while (numerosUsuario.Count < 6)
+            {
+
+                numero = int.Parse(Interaction.InputBox("Introduce un numero"));
+                if (numero > 0 && numero < 50)
+                {
+                    if (!numerosUsuario.Contains(numero))
+                    {
+                        numerosUsuario.Add(numero);
+                    }
+                    else MessageBox.Show("El numero ya ha sido introducido, prueba otro");
+                }
+                else
+                {
+                    MessageBox.Show("El numero introducido no es valido, prueba otro");
+                }
+
+
+            }
+
+
+        }
+
+        private void bNumerosAcertados_Click(object sender, EventArgs e)
+        {
+            string texto; int numero; //int numero2;
+            texto = mostrarlistasnumeros(numerosSorteo, numerosUsuario);
+            MessageBox.Show(texto);
+
+            //Este es facilitado en la corrección
+            //numero2 = numerosAcertados2(numerosSorteo, numerosUsuario);
+            //MessageBox.Show("Los numeros acertados por el usuario en el sorteo han sido de: " + numero2);
+
+            //Aparte del que me facilitaste, para no hacer lo mismo he implementado otro nuevo que creo que es lo que me pedia el ejercicio
+            numero = numerosAcertadosSorteo(numerosSorteo, numerosUsuario);
+            MessageBox.Show("Los numeros acertados por el usuario en el sorteo han sido de: " + numero);
+            
+            
+        }
+
+        private string mostrarlistasnumeros(List<int> numeroSorteo, List<int> numeroUsuario)
+        {
+            string texto = "";
+
+            for (int i = 0; i < numeroSorteo.Count; i++)
+            {
+                texto = "La lista de los aleatorios son :  " + montarlista(numeroSorteo) + "\n";
+                texto = texto + "La lista de los usuario son: " + montarlista(numeroUsuario);
+            }
+
+            return texto;
+        }
+
+        private string montarlista(List<int> lista)
+        {
+            string texto = "";
+            for (int i = 0; i < lista.Count; i++)
+            {
+                texto = texto + lista[i] + ",";
+
+            }
+
+            return texto;
+        }
+
+        private int numerosAcertadosSorteo(List<int> numeroSorteo, List<int> numeroUsuario)
+        {
+            int numerosAcertados = 0;
+            int j = 0;
+            
+            for(int i = 0; i < numeroSorteo.Count; i++)
+            {
+                while(j < numeroSorteo.Count)
+                {
+                    if (numeroUsuario[i] == numeroSorteo[j])
+                    {
+                        numerosAcertados++;
+                    }
+                    j++;
+                }
+                j = 0;
+            }
+            return numerosAcertados;
+        } 
+
+        private int numerosAcertados2(List<int> numeroSorteo, List<int> numeroUsuario)
+        {
+            int cont = 0;
+
+            for (int i = 0; i < numerosSorteo.Count; i++)
+            {
+                if (numerosUsuario.Contains(numerosSorteo[i]))
+                {
+                    cont++;
+                }
+            }
+
+            return cont;
+        }
+    }
+
+}
